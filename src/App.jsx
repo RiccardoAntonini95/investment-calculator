19adee8ef3f66de9b5d33d9869775575b2eb10cb
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-07-25 16:43:28 +0200

    setup project + header component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/header"
+
+function App() {
+  return (
+    <>
+    <h1>React Investment Calculator</h1>
+    <Header />
+    <p>inputs</p>
+    <p>data</p>
+    </>
+  )
+}
+
+export default App
